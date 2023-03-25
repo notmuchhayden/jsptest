19d9ab1f2bbc3e72a8ae1f4dc21c3445f5abc18b
seaeast2 <seaeast2@gmail.com>
2023-03-25 14:31:47 +0900

    JSP template

--- src/main/java/com/example/jsptest/controller/JspController.java
+++ src/main/java/com/example/jsptest/controller/JspController.java
@@ -1,13 +1,14 @@
 package com.example.jsptest.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class JspController {
     @GetMapping("/")
-    public String hello() {
-
+    public String hello(Model model) {
+        model.addAttribute("message", "환영합니다. JSP - Srping 연동이 성공했습니다.");
         return"index";
     }
 }
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -19,7 +19,8 @@
 
 <body>
     <h1>JSP Hello!</h1>
-    <h1>현재시간은 <%=hour %>시 <%=minute %>분 <%=second %>초 입니다.</h1>
-    <c:out value="Hello World" />
+    <h1>현재시간은 <%= hour %>시 <%=minute %>분 <%=second %>초 입니다.</h1>
+    <c:out value="JSTL test : Hello World" />
+    <h2>EL Test : ${message}</h2>
 </body>
 </html>
